5c2c5eed2b7ca658a793dc4166fe5bcaaef2b5ca
Devansh27112001 <dkansara2711@gmail.com>
2025-09-02 10:44:10 -0400

    consuming the message produced by payment-successful service

---
+++ analytic-service/index.js
@@ -0,0 +1,31 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "analytic-service",
+  brokers: ["localhost:9094"],
+});
+
+const consumer = kafka.consumer({ groupId: "analytic-service" });
+
+const run = async () => {
+  try {
+    await consumer.connect();
+    await consumer.subscribe({
+      topic: "payment-successful",
+      fromBeginning: true,
+    });
+
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        const value = message.value.toString();
+        const { userId, cart } = JSON.parse(value);
+
+        const totalVal = cart.reduce((acc, curr) => acc + curr.price, 0);
+        console.log(`Analytic consumer: User ${userId} paid ${totalVal}`);
+      },
+    });
+  } catch (error) {
+    console.log("Error connecting to kafka", error);
+  }
+};
+run();

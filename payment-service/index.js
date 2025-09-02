5a057c151b2a5805488fb8e59d8d3dca142a73fa
Devansh27112001 <dkansara2711@gmail.com>
2025-09-01 23:09:28 -0400

    Initial commit for payment-service

--- payment-service/index.js
+++ payment-service/index.js
@@ -11,10 +11,18 @@ app.use(express.json());
 
 app.post("/payment-service", async (req, res) => {
   const { cart } = req.body;
-  // ASSUMEN THAT WE ARE GETTING THE USERid and THE DECRPYTED COOKIE
+  console.log(cart);
+  // ASSUMEING THAT WE ARE GETTING THE USERid and THE DECRPYTED COOKIE
+  const userId = "123";
+
+  // TODO: PAYMENT
+  console.log("API endpoint hit");
+  // KAFKA
+
+  return res.status(200).send("Payment successful");
 });
 app.use((err, req, res, next) => {
-  res.status(err.status || 5000).send(err.message);
+  res.status(err.status || 500).send(err.message);
 });
 app.listen(8000, () => {
   console.log("Payment service is running on PORT 8000");

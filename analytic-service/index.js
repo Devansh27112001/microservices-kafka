57f591e9af087be11fcc7435a22a68ea7fa331ad
Devansh27112001 <dkansara2711@gmail.com>
2025-09-05 09:05:54 -0400

    Implementing the final flow

--- analytic-service/index.js
+++ analytic-service/index.js
@@ -11,12 +11,39 @@ const run = async () => {
   try {
     await consumer.connect();
     await consumer.subscribe({
-      topic: "payment-successful",
+      topics: ["payment-successful", "email-successful", "order-successful"],
       fromBeginning: true,
     });
 
     await consumer.run({
       eachMessage: async ({ topic, partition, message }) => {
+        switch (topic) {
+          case "payment-successful":
+            {
+              const paymentValue = message.value.toString();
+              const { userId, cart } = JSON.parse(value);
+
+              const totalVal = cart.reduce((acc, curr) => acc + curr.price, 0);
+              console.log(`Analytic consumer: User ${userId} paid ${totalVal}`);
+            }
+            break;
+          case "order-successful":
+            {
+              const ordervalue = message.value.toString();
+              const { userId, orderId } = JSON.parse(ordervalue);
+
+              console.log(`Analytic consumer: User ${userId} paid ${totalVal}`);
+            }
+            break;
+          case "email-successful":
+            {
+              const emailValue = message.value.toString();
+              const { userId, emailId } = JSON.parse(emailValue);
+            }
+            break;
+          default:
+            break;
+        }
         const value = message.value.toString();
         const { userId, cart } = JSON.parse(value);
 
---
+++ order-service/index.js
@@ -0,0 +1,41 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "order-service",
+  brokers: ["localhost:9094"],
+});
+
+const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: "order-service" });
+
+const run = async () => {
+  try {
+    await producer.connect();
+    await consumer.connect();
+    await consumer.subscribe({
+      topic: "payment-successful",
+      fromBeginning: true,
+    });
+
+    await consumer.run({
+      eachMessage: async ({ topic, message, partition }) => {
+        const value = message.value.toString();
+        const { userId, cart } = JSON.parse(value);
+
+        // TODo: Create the order on DB
+        const dummyorderId = "4568978";
+        console.log(
+          `Order consumer: Order created successfully for user ${userId} with orderId ${dummyorderId}`
+        );
+        await producer.send({
+          topic: "order-successful",
+          messages: [
+            { value: JSON.stringify({ userId, orderId: dummyorderId }) },
+          ],
+        });
+      },
+    });
+  } catch (error) {
+    console.log("Error connecting to kafka", error);
+  }
+};
